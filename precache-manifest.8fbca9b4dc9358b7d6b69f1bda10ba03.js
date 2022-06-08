3d774c66b5f0c34a190a9993c11fb458405f1a53
mooenz <joss92821@hotmail.com>
2022-06-08 12:14:59 -0500

    Updates

---
+++ precache-manifest.8fbca9b4dc9358b7d6b69f1bda10ba03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d10d6a81730dfec9e64e",
+    "url": "/weather-app-master/static/js/main.d10d6a81.chunk.js"
+  },
+  {
+    "revision": "0f463367b77a8d8f6aec",
+    "url": "/weather-app-master/static/js/1.0f463367.chunk.js"
+  },
+  {
+    "revision": "8b8ab528bcbad8ca7fa9",
+    "url": "/weather-app-master/static/js/runtime~main.8b8ab528.js"
+  },
+  {
+    "revision": "11452cab98ccf2a171a36da25360ba1b",
+    "url": "/weather-app-master/static/media/Cloud-background.11452cab.png"
+  },
+  {
+    "revision": "a8528a868d461f09ee29a47e4c5323c8",
+    "url": "/weather-app-master/static/media/Clear.a8528a86.png"
+  },
+  {
+    "revision": "c6d1cb3616b92442a99eb3acd4063a89",
+    "url": "/weather-app-master/static/media/Haze.c6d1cb36.png"
+  },
+  {
+    "revision": "b62e950be97488071f86695792d1a919",
+    "url": "/weather-app-master/static/media/Shower.b62e950b.png"
+  },
+  {
+    "revision": "e1202bb6505e7b4c7a9fac14eafe4162",
+    "url": "/weather-app-master/static/media/Sleet.e1202bb6.png"
+  },
+  {
+    "revision": "7618eb85fb278b1268d19d9e4dba6592",
+    "url": "/weather-app-master/index.html"
+  }
+];
